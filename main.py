aa8ef5664dc0e007c2854f33ffc0cb104fdbb356
raymondhung010 <raymondhung010@gmail.com>
2022-03-27 19:42:36 +0800

    install paddleocr dependencies and add logic to extract text from opencv result

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
 from helper import WindowCapture
 import cv2 as cv
+from paddleocr import PaddleOCR
 
 # ==============================================================================
 if __name__ == '__main__':
@@ -8,6 +9,7 @@ if __name__ == '__main__':
     """
         main method 
     """
+
     rbWindowText = WindowCapture.find_ros_window_names()
     # debug mode will be in the first position
     # debug mode window text always be 10 random string
@@ -18,7 +20,9 @@ if __name__ == '__main__':
         crop_screenshot = screenshot[60:round(pre_crop_h / 2), 0:round(pre_crop_w / 3)]
         (cropped_h, cropped_w) = crop_screenshot.shape[:2]
         img = cv.resize(crop_screenshot, (cropped_w * 2, cropped_h * 2))
-        cv.imshow("resize", img)
-        cv.imwrite("test2.png", img)
-        cv.waitKey(0)
+        ocr = PaddleOCR(use_angle_cls=True, lang='en')
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        result = ocr.ocr(img, cls=False)
+        for line in result:
+            print(line)
 
